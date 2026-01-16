216f4fafd9e5066e0bd436ad4426e99185d4fbba
IngridKao <ingrid-kao@qware.com>
2026-01-16 11:46:58 +0800

    新增購物車

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -2,10 +2,20 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCartStore = defineStore('cart', () => {
-  const items = ref([1, 2, 3])
+  const items = ref([])
   const cartCount = computed(() => items.value.length)
-  const add = () => {
-    items.value.push(cartCount.value + 1)
+  // val：父層傳進來的商品資料
+  // prod: 購物車裡面的每一個商品
+  const add = (val) => {
+    const target = items.value.find((prod) => prod.id === val.id)
+    if (!target) {
+      items.value.push({
+        ...val,
+        count: 1,
+      })
+    } else {
+      target.count++
+    }
   }
   const clear = () => {
     items.value = []
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,30 @@
 <script setup>
-// import { useCounterStore } from '@/stores/counter'
-// const counter = useCounterStore()
 import { useCartStore } from '@/stores/cart'
 const cartStore = useCartStore()
+// 範例商品資料（之後可換成 API 取回來）
+const products = [
+  { id: 1, name: '咖啡', price: 120 },
+  { id: 2, name: '吐司', price: 60 },
+  { id: 3, name: '紅茶', price: 45 },
+]
 </script>
 <template>
   <main>
-    <p v-for="cart in cartStore.items">{{ cart }}</p>
-    <button @click="cartStore.add()">加入購物車</button>
-    <button @click="cartStore.clear()">清空</button>
-    <!-- <p>{{ counter.count }}</p>
-    <p>{{ counter.doubleCount }}</p>
-    <button @click="counter.increment()">嘿嘿</button> -->
+    <div>
+      <div v-for="prod in products" :key="prod.id">
+        {{ prod.id }}. {{ prod.name }} $ {{ prod.price }}
+        <button @click="cartStore.add(prod)">加入購物車</button>
+      </div>
+    </div>
+    <aside>
+      <p v-for="cart in cartStore.items" :key="cart">{{ cart }}</p>
+      <button @click="cartStore.clear()">清空</button>
+    </aside>
   </main>
 </template>
+
+<!-- <style lang="scss" scoped>
+main {
+  display: inlne-flex;
+}
+</style> -->
